ba6eb80d8bb30366b58c4cc51bba611590c4acbc
Albert Fit <af@albert-fit.com>
2024-08-01 11:14:29 +0100

    added throttler

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); //add configuration object to limit access to cors ( otherwise open to public )
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
